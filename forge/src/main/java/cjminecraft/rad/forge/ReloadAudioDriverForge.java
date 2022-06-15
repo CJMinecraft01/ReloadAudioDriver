3f477c206e18f73019ab79ba5ad902a298c3f5a8
JayJay1989BE <jonas@lateur.pro>
2022-06-15 19:09:36 +0200

    Port to 1.16

--- forge/src/main/java/cjminecraft/rad/forge/ReloadAudioDriverForge.java
+++ forge/src/main/java/cjminecraft/rad/forge/ReloadAudioDriverForge.java
@@ -6,17 +6,18 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod(ReloadAudioDriver.MOD_ID)
 @Mod.EventBusSubscriber
 public class ReloadAudioDriverForge {
     public ReloadAudioDriverForge() {
         ReloadAudioDriver.init();
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
     }
 
     @SubscribeEvent
